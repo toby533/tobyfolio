9f2e5bb561f3b862a8c9e083e8e818b63109ce2c
Thibault Drevon <thibaultdrevon@MacBook-Pro-G4G.mshome.net>
2020-10-08 18:03:59 +0200

    fix node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,21 +10,3 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
     })
   }
 }
-exports.createPages = async ({ graphql, actions }) => {
-  // **Note:** The graphql function call returns a Promise
-  // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise for more info
-  const result = await graphql(`
-    query {
-      allMarkdownRemark {
-        edges {
-          node {
-            fields {
-              slug
-            }
-          }
-        }
-      }
-    }
-  `)
-  console.log(JSON.stringify(result, null, 4))
-}
